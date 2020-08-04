d4f841acb72eaee1fca01558243e18b648b8c5c2
valerio <valerio.deprosperi@gmail.com>
2020-08-04 15:56:13 +0200

    var prezzo biglietto km utente

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 // chiedera utente numero km che vuole percorrere ed età passegero
-var kmUtente
-var etàUtente
+var kmUtente;
+var etàUtente;
 kmUtente = prompt ("quanti chilometri vorresti percorrere?");
 etàUtente = prompt ("quanti anni hai?");
+// prezzo tratta in base ai km
+var prezzBiglietto
+prezzBiglietto = kmUtente * 0.21;
